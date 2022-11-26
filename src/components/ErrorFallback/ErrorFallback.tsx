11c72e03c5fce8a2308e97e97ef67fd7ff2c12c1
Tasos Kyr <tasoskyr1@gmail.com>
2022-11-26 20:43:19 +0100

    feat: error handling

---
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -0,0 +1,23 @@
+import { FallbackProps } from "react-error-boundary"
+export function FullPageErrorFallback() {
+  return (
+    <div
+      role="alert"
+      className="h-screen text-red-600 bg-red-200 flex justify-center"
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+    </div>
+  );
+}
+
+export function ErrorMessage({error}: FallbackProps) {
+  return (
+    <div
+      role="alert"
+      className="h-screen text-red-600 bg-red-200 flex justify-center"
+    >
+      <span>There was an error: </span>
+      <pre>{error.name}: {error.message}</pre>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,10 @@ import Layout from "src/components/Layout";
 import "../styles/globals.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ErrorBoundary } from "react-error-boundary";
-
+import {
+  FullPageErrorFallback,
+  ErrorMessage,
+} from "src/components/ErrorFallback/ErrorFallback";
 export type NextPageWithLayout<P = Record<string, never>, IP = P> = NextPage<
   P,
   IP
@@ -28,15 +31,15 @@ const queryClient = new QueryClient({
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   return (
-    <ErrorBoundary 
-    // FallbackComponent={FullPageErrorFallback}
-    >
-    <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ErrorBoundary FallbackComponent={FullPageErrorFallback}>
+      <QueryClientProvider client={queryClient}>
+        <ErrorBoundary FallbackComponent={ErrorMessage}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ErrorBoundary>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ErrorBoundary>
   );
 }
